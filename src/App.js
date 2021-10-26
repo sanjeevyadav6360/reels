bec8d2b2aa1c675fa9d92a3a79373fadbcdd6128
sanjeevyadav6360 <sanjeevyadav6360@gmail.com>
2021-10-26 11:46:56 +0530

    reels

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/home";
+import Login from "./components/login";
+import AuthProvider from "./AuthProvider";
+function App() {
+    return (
+        <>
+            <AuthProvider>
+                <Router>
+                    <Switch>
+                        <Route exact path="/login">
+                            <Login />
+                        </Route>
+                        <Route exact path="/">
+                            <Home />
+                        </Route>
+                    </Switch>
+                </Router>
+            </AuthProvider>
+        </>
+    );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/compat/app";
+import config from "./config.json";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+import "firebase/compat/storage";
+
+firebase.initializeApp(config);
+
+let provider = new firebase.auth.GoogleAuthProvider();
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+export const storage = firebase.storage();
+
+export const signInWithGoogle = () => {
+  auth.signInWithPopup(provider);
+};
+
+export default firebase;
